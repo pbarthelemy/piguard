4f4980c0edff1467f4fa23f1393d49bbbcd75309
pbarthelemy <30569083+pbarthelemy@users.noreply.github.com>
2018-08-25 13:13:51 -0400

    Update args.py

--- scratch/args.py
+++ scratch/args.py
@@ -1,4 +1,7 @@
 import argparse
+import requests
+import HartlandPiconfig as cfg
+
 
 parser = argparse.ArgumentParser(description='sendpic parameters')
 parser.add_argument('-t', '--text',  action='store' , nargs=1,required=True ,
@@ -7,5 +10,20 @@ parser.add_argument('-u', '--url',  action='store' , nargs=1,required=True ,
                     help='URL of the image')
 
 args = parser.parse_args()
-print args.text
-print args.url
+print(args.text)
+print(args.url)
+
+rq = cfg.synoconst
+rq.update(cfg.synotoken)
+
+payload = '{"text": "' + args.text + '": "'+args.url+'"}'
+print(payload)
+
+rq['payload'] = '{"text": "' + args.text + '": "'+args.url+'"}'
+
+r = requests.get(cfg.synoURL, params=rq)
+
+print(r.url)
+print(r.status_code)
+print(r.text)
+print(r.json)
